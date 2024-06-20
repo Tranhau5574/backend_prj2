737392f48e31ff3b890e2988a6da914b969f3210
Tranhau5574 <tranvanhaubg2003@gmail.com>
2024-06-20 11:32:54 +0700

    ...

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 
 )
 //Port || 5000
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`);
 })
