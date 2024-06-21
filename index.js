1bea1d8b508ff4e5f6b443a7e94789da1a00f9ee
Tranhau5574 <tranvanhaubg2003@gmail.com>
2024-06-21 10:23:18 +0700

    h

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 app.use(express.json());
-app.use(cors());
 app.use(
   cors({
     origin: (origin, callback) => {
-      const allowedOrigins = [process.env.FRONTEND_URL, "https://backend-prj2.onrender.com", "http://localhost:5173", "http://localhost:5174"];
+      const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173", "http://localhost:5174"];
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
